543c98fa5c07b50b65bbf14335440664fcc8cd89
lollolzz <lollolzz1018@gmail.com>
2021-12-31 20:43:11 +0900

    수정 중

--- main.js
+++ main.js
@@ -30,10 +30,10 @@ navbarToggleBtn.addEventListener('click', () => {
 });
 
 // Handle click on "contact me" button on home
-const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click', () => {
-  scrollIntoView('#contact');
-});
+// const homeContactBtn = document.querySelector('.home__contact');
+// homeContactBtn.addEventListener('click', () => {
+//   scrollIntoView('#contact');
+// });
 
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
